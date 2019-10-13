fd8c27160c0a7a9ff19aa429a41d96b44ee8c6f5
AlShabibi <shabams@gmail.com>
2019-10-13 17:47:38 +0300

    working on whoWon()

--- app.js
+++ app.js
@@ -13,15 +13,36 @@ const r3c2 = document.querySelector(".r-3-c-2");
 const r3c3 = document.querySelector(".r-3-c-3");
 
 let firstPlayerTurn = true;
+const map = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
 const play = e => {
-  console.log(event.target);
-  if (firstPlayerTurn == true) {
-    e.target.querySelector(".tic").style.display = "flex";
+  const circle = e.target.querySelector(".tic");
+  const x = e.target.querySelector("p");
+  const location = e.target
+    .getAttribute("class")
+    .split(" ")[0]
+    .split("-");
+  console.log(location);
+  const row = location[1];
+  const column = location[3];
+
+  if (firstPlayerTurn == true && circle) {
+    circle.style.display = "flex";
+    map[row - 1][column - 1] = 1;
+    console.log(map);
+    if (x) x.remove();
     firstPlayerTurn = !firstPlayerTurn;
-  } else {
+  } else if (firstPlayerTurn == false && x) {
+    x.style.display = "flex";
+    map[row - 1][column - 1] = 2;
+    if (circle) circle.remove();
+    firstPlayerTurn = !firstPlayerTurn;
   }
 };
+
+const whoWon = () => {};
+
+for (let i = 0; i < 3; i++) {}
 
 r1c1.addEventListener("click", play);
 r1c2.addEventListener("click", play);
